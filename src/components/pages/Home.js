c978595ec0f19e188e980271aa45e043fcfb9942
nhanlt81 <ntl2@pdx.edu>
2021-05-14 10:23:51 -0700

    Updated Navbar
    Changed color Navbar background when we hover over a link.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import '../../App.css';
+import React from "react";
+import "../../App.css";
 
 export default function Home() {
   return (
     <>
-      <h1 className='home'>EPIC</h1>
+      <h1 className="home">Home</h1>
     </>
   );
 }
