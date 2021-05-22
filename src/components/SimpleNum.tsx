f6b217e75f15d60250d083199f78614a6b530af6
Daniel Dupriest <kououken@gmail.com>
2021-05-22 13:25:37 -0700

    Implemented SimpleNum component, which simplifies long numbers into a text string.

---
+++ src/components/SimpleNum.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+interface SimpleNumProps {
+  value: number,
+}
+
+function SimpleNum(props: SimpleNumProps) {
+
+  //Code from https://stackoverflow.com/questions/10599933/convert-long-number-into-abbreviated-string-in-javascript-with-a-special-shortn
+  function abbreviateNumber(value) {
+    var newValue = value;
+    if (value >= 1000) {
+        var suffixes = ["", "K", "M", "B","T"];
+        var suffixNum = Math.floor( (""+value).length/3 );
+        var shortValue:number = 0;
+        for (var precision = 2; precision >= 1; precision--) {
+            shortValue = parseFloat( (suffixNum != 0 ? (value / Math.pow(1000,suffixNum) ) : value).toPrecision(precision));
+            var dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g,'');
+            if (dotLessShortValue.length <= 2) { break; }
+        }
+        var shortValueString = shortValue+''
+        if (shortValue % 1 != 0)  shortValueString = shortValue.toFixed(1)+'';
+        newValue = shortValueString+' '+suffixes[suffixNum];
+    }
+    return newValue;
+  }
+
+  const { value } = props
+  return (
+    <span className="simpleNum">
+      {abbreviateNumber(value)}
+    </span>
+  )
+}
+
+export default SimpleNum
