136e1a76fbb7aef5f491139f71a3627fa9710024
nhanlt81 <ntl2@pdx.edu>
2021-05-23 17:13:52 -0700

    Create ExchangeStatusUpdate.tsx

---
+++ src/components/ExchangeStatusUpdate.tsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import './ExchangeStatusUpdate.css'
+import parse from 'html-react-parser'
+interface ExchangeStatusUpdateProps{
+    status_updates:StatusUpdate[],
+}
+interface Image{
+    thumb: string,
+    small: string,
+    large: string,
+}
+interface Project{
+    type:string,
+    id:string,
+    name:string,
+    image: Image
+}
+interface StatusUpdate{
+    description:string,
+    category: string,
+    created_at:Date,
+    user:string,
+    user_title:string,
+    pin:boolean,
+    project:Project
+
+}
+export class ExchangeStatusUpdate extends Component<ExchangeStatusUpdateProps> {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
+    render() {
+        const {status_updates} = this.props;
+        console.log(status_updates)
+        return (
+            <div> News:
+                {status_updates.map((status, i) => 
+                    <div className="status-container" key={i+1}>
+                      <div className="status-title">Title:{status.user_title}</div>
+                      <div className="status-user"><div className="user-name">User:{status.user}</div><div className="created-at">Created at:{status.created_at}</div></div>
+                      <div className="status-description">Description:{parse(status.description)}</div>
+                    </div>  
+                  )}
+            </div>
+        )
+    }
+}
+
+export default ExchangeStatusUpdate
