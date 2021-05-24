e515670610d17ce4e769d1b325ccaebe66ff379a
nhanlt81 <ntl2@pdx.edu>
2021-05-24 09:49:24 -0700

    Update ExchangeTable.tsx

--- src/components/ExchangeTable.tsx
+++ src/components/ExchangeTable.tsx
@@ -82,8 +82,10 @@ export class ExchangeTable extends Component<ExchangeTableProps> {
                         <tr key={i+1}>
                             <td key={0}>{i+1}</td>
                             <td key={1}>{<CoinName id={ticker.coin_id}/>}</td>
+                            {/* <td key={1}><Link className="base-coin" to={"/coin/" + ticker.coin_id}>{ticker.base}</Link></td> */}
                             <td key={2}><a href={ticker.trade_url}>{ticker.base}/{ticker.target}</a></td>
                             <td key={3}><CoinPrice id={ticker.coin_id} currency={currency} /></td>
+                            {/* <td key={3}>{ticker.last}</td> */}
                             <td key={4}>{ticker.bid_ask_spread_percentage * 100} %</td>
                             <td key={7}>${ticker.converted_volume.usd}</td>
                             <td key={8}>{new Date(ticker.last_traded_at).toLocaleString()}</td>
