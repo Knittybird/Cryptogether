df23ccfdf971fe056f3bf540b15e6af4f1fee365
nhanlt81 <ntl2@pdx.edu>
2021-05-24 23:37:18 -0700

    Update CoinDescription.tsx

--- src/components/CoinDescription.tsx
+++ src/components/CoinDescription.tsx
@@ -18,9 +18,9 @@ export class CoinDescription extends Component<CoinDescriptionProps> {
     render() {
         const {name,description} = this.props;
         return (
-            <div>
-                <div className="description-title">Name: {name}</div>
-                <div className="description-content">Description: {parse(description)}</div>
+            <div className="coin-description-container">
+                <div className="title">About {name}</div>
+                <div className="content">Description: {parse(description)}</div>
             </div>
         )
     }
