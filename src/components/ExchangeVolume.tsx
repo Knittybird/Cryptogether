84aa12b9c873ee581c471ad07a5562a148801b9a
Robin <rgearn@pdx.edu>
2021-05-26 12:00:53 -0700

    errors with live data

--- src/components/ExchangeVolume.tsx
+++ src/components/ExchangeVolume.tsx
@@ -12,14 +12,30 @@ interface VolumeChartProps{
 export default function VolumeChart({ids, names, title}:VolumeChartProps){
   const [Title, setTitle] = useState<string> ();
   const [series, setSeries] = useState<ApexAxisChartSeries>([])
+  
+  useEffect(() => {
+    let volume_series:ApexAxisChartSeries = []
+    ids.forEach((id, index) => {
+    const url = `https://api.coingecko.com/api/v3/exchanges/${id}/volume_chart?days=7`;
+      axios.get(url)
+          .then(response => {
+            const v_data:[number, number][] = response.data.map((id) => [id[0], parseInt(id[1])])
+            volume_series.push({data: v_data, name: names[index]})
+          })
+          .catch((error) => {console.log("Something went wrong. ", error)})
+          })
+    setSeries(volume_series)
+    if (title) {
+      setTitle(title)
+    }
+  }, [series, title])
 
   return (
     <>
-      <h3>{title}</h3>
-      <p>{ids}, {names}</p>
-    {/* {series &&(
-      // <LineChart series={series} title={title}></LineChart>
-    )} */}
+      <p>  {ids}, {names}</p>
+    {series &&(
+      <LineChart series={series} title={title}></LineChart>
+    )}
     </>
   )
 }
